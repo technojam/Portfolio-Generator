355060cd9fd0451d0485ba9346b63aacca9b96dd
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2020-10-01 03:20:40 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-//dev keys
-module.exports = {
-    googleClientID: "852520680343-sc5jd37cs8iot8q0def88lrf7tpplup1.apps.googleusercontent.com",
-    googleClientSecret: "ZK5sCMdWVmG51SH0QaL21n36",
-    mongoURI: "mongodb+srv://Shivansh:dojo@cluster0.n0cr0.mongodb.net/Clusters?retryWrites=true&w=majority",
-    cookieKey: "vhgmvcfhgcfvh",
-    FACEBOOK_APP_ID: "2681764865485987",
-    FACEBOOK_APP_SECRET: "1ad90e4c523f293d62c207caef54aa22",
-};
