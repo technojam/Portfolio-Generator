49594790acef98aa4f7a4a7a95c8d17faaaf414a
sidhearts <rajsiddharth21@gmail.com>
2020-10-05 12:12:52 +0530

    landing page

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 import Header from './Header';
+import Landing from './LandingComponent';
 
 class Main extends Component {
     render() {
         return (
             <div>
-               <Header/> 
+                <Landing />
+               {/* <Header/>  */}
                {/* <Route exact path="/" component={()=> <Home/>}/>
                <Route path="/home" component={()=> <Home/>}/>
                <Route path="/about" component={()=> <About/>}/>
---
+++ client/src/components/images.js
@@ -0,0 +1,8 @@
+export const images = {
+    portfolio1 : require('../images/portfolio1.png'),
+    portfolio2 : require('../images/portfolio2.png'),
+    portfolio3 : require('../images/portfolio3.png'),
+    portfolio4 : require('../images/portfolio4.png'),
+    portfolio5 : require('../images/portfolio5.png'),
+    portfolio6 : require('../images/portfolio6.png'),
+}
